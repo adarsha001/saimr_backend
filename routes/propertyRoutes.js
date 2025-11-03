196fbe0d8d46ce73d5f92ece26406d36cd120f78
adarshaharshavardhana@gmail.com <adarshaharshavardhana@gmail.com>
2025-11-04 02:10:27 +0530

    kept the create property outside

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -9,8 +9,8 @@ router.get("/", getProperties);
 router.get("/:id", getPropertyById);
 
 // Protected routes - require authentication
-router.use(protect); // This applies to all routes below
 router.post("/", upload.array("images", 10), createProperty);
+router.use(protect); // This applies to all routes below
 router.get('/my-properties', getPropertiesByUser);
 
 module.exports = router;
